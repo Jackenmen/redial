ed06257e06aedc9e1d51a1d1e11341479c32bd75
Jakub Kuczys <me@jacken.men>
2025-05-16 05:23:24 +0200

    Switch to pyproject.toml

--- setup.py
+++ setup.py
@@ -13,12 +13,6 @@ import sys
 from pkg_resources import require, VersionConflict
 from setuptools import setup
 
-try:
-    require('setuptools>=38.3')
-except VersionConflict:
-    print("Error: version of setuptools is too old (<38.3)!")
-    sys.exit(1)
-
 
 if __name__ == "__main__":
     setup(use_pyscaffold=True, long_description_content_type='text/markdown')
